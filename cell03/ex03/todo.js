1fb134093435f4abfa1ddd2a1f1dcc3651a71e9d
Matthieu Larboullet 42 <mlarboul@student.42.fr>
2022-04-29 22:30:16 +0200

    feat COOKIES OK

--- cell03/ex03/todo.js
+++ cell03/ex03/todo.js
@@ -1,18 +1,21 @@
-let cookies = [];
-
-function createDiv(task) {
+function createDiv(task, uid) {
     // Create the div_child
     let element = document.createElement("div");
     element.classList.add('task');
+    if (uid == 0)
+        uid = Date.now()
+
+    element.setAttribute('data-todo', uid);
 
     // remove element onclick;
     element.onclick = function () {
-        if (window.confirm("Are you sure you want to delete \"" + this.textContent + "\" ?"))
-        {
+    if (window.confirm("Are you sure you want to delete \"" + this.textContent + "\" ?"))
+        {            
+            // remove the div cliked on
             this.parentElement.removeChild(this);
-            let index = cookies.indexOf(this.textContent);
-            cookies.splice(index, 1);
-            document.cookie = cookies;
+
+            // remove the right cookie
+            removeCookies(this.getAttribute('data-todo'));
         }
     };
 
@@ -22,21 +25,43 @@ function createDiv(task) {
     // Append the div_child to the div_parent
     document.getElementById('ft_list').insertBefore(element, document.getElementsByClassName('task')[0]);
 
-    cookies.push(element.textContent);
-    document.cookie = cookies;
+    addCookie(element.textContent, uid)
 }
 
 function newTask() {
     let task = prompt("Enter the new element");
-    createDiv(task);
+    if (task)
+        createDiv(task, 0);
 }
 
-function init() {
-    let str = document.cookie;
-    const cookies = str.split(',');
-    for (const element of cookies) {
-        createDiv(element);
-        }
+
+function restoreCookies() {
+    const cookies = document.cookie.split(";");
+    for (const cookie of cookies) {
+        const value = cookie.split("=")
+        createDiv(value[1], value[0]);
+    }
 }
 
-window.onload = init();
+
+function addCookie(cvalue, ckey) {
+    var now = new Date();
+    now.setTime(now.getTime() + 1 * 3600 * 1000);
+
+    document.cookie= ckey + "=" + cvalue + ";expires=" + now.toUTCString() + "; SameSite=None; Secure";
+ }
+
+ function removeCookies(key_to_del) {
+    var res = document.cookie;
+    var multiple = res.split(";");
+    for(var i = 0 ; i < multiple.length; i++) {
+       var key = multiple[i].split("=");
+       key[0] = parseInt(key[0]);
+       if (key[0] == key_to_del)
+       {
+        document.cookie=key[0]+"=; expires=Thu, 01 Jan 1970 00:00:00 UTC; SameSite=None; Secure";
+    }
+    }
+ }
+
+window.onload = restoreCookies();
